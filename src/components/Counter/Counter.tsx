b5de8a0e8602dca6391b7772f75ae917c5ec4c99
James O'Kane <orbiccode@gmail.com>
2025-10-03 20:06:45 +0100

    increment, decrement

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,7 +1,26 @@
+import { useState } from 'react';
+
 export default function Counter() {
+  const [count, setCount] = useState<number>(0);
+
+  function increment(): void {
+    setCount((prev: number): number => prev + 1);
+  }
+
+  function decrement(): void {
+    if (count > 0) {
+      setCount((prev: number): number => prev - 1);
+    }
+  }
+
   return (
     <>
       <h1>Counter</h1>
+      <p>{count}</p>
+      <div>
+        <button onClick={increment}>Increment +</button>
+        <button onClick={decrement}>Decrement -</button>
+      </div>
     </>
   );
 }
